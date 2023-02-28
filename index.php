97cee0783316ab1e1370f32e29ed294d1b437455
Antoine de Mareschal <toto.63146@gmail.com>
2023-02-28 12:30:49 +0100

    Few changes

--- index.php
+++ index.php
@@ -4,6 +4,7 @@
  * Déclaration du tableau des recettes
  * Chaque élément du tableau est un tableau numéroté (une recette)
  */
+
 $recipes = [
     ['Cassoulet','[...]','mickael.andrieu@exemple.com',true,],
     ['Couscous','[...]','mickael.andrieu@exemple.com',false,],
