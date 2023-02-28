0921fc6d6f84b68873aaf85fa837db468d634eb9
Antoine de Mareschal <toto.63146@gmail.com>
2023-02-28 11:37:42 +0100

    Few changes

--- index.php
+++ index.php
@@ -3,4 +3,7 @@ $recipe['title'] = 'Cassoulet';
 $recipe['recipe'] = 'Etape 1 : des flageolets, Etape 2 : ...';
 $recipe['author'] = 'john.doe@exemple.com';
 $recipe['enable'] = true;
+
+echo $recipe['title'];
+
 ?>
